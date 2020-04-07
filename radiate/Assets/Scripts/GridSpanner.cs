4d506afaeef1f445794a4f5af3d81369b191edc8
Gergő Balogh <geryxyz@gmail.com>
2020-04-07 16:16:57 +0200

    very basic tiling

---
+++ radiate/Assets/Scripts/GridSpanner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpanner : MonoBehaviour
+{
+    public Vector2Int GridSize;
+    public GameObject Tile;
+    public float Gap;
+
+    void Start()
+    {
+        for (int x = 0; x < GridSize.x; x++)
+        {
+            for (int y = 0; y <GridSize.y; y++)
+            {
+                var instance = Instantiate(Tile);
+                instance.transform.position = new Vector3(
+                    (instance.GetComponent<Renderer>( ).bounds.size.x + Gap) * x,
+                    0,
+                    (instance.GetComponent<Renderer>( ).bounds.size.z + Gap) * y);
+            }
+        }
+    }
+}
